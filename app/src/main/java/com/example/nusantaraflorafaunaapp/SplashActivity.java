f4caeeeb4d23677932369a50ba53b73127e3047c
Muhammad Prayogo Pangestu <prayogoxyz@gmail.com>
2026-06-26 21:53:50 +0700

    feat: add splash screen and fix status bar safe area

---
+++ app/src/main/java/com/example/nusantaraflorafaunaapp/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.nusantaraflorafaunaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2500);
+    }
+}
